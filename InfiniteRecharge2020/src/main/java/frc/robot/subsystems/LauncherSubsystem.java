bba5bf66b0910c7aa8640f4665f90c4dd591cbcd
ricozhuthegreat <ricozhuthegreat@hotmail.com>
2020-01-13 16:46:07 -0500

    Added Documentation on LauncherSubsystem.java

--- InfiniteRecharge2020/src/main/java/frc/robot/subsystems/LauncherSubsystem.java
+++ InfiniteRecharge2020/src/main/java/frc/robot/subsystems/LauncherSubsystem.java
@@ -1,14 +1,20 @@
 /*----------------------------------------------------------------------------
 
-Author: Rico Zhu                Date: January 13th, 2019
+Author: Rico Zhu, Emily DiLauro and Nolan Smyth              Date: January 13th, 2019
 
 LauncherSubsystem Class
+
+This is a launcher subsytem called the LancherSubsystem.
+It packages the frc.robot.subsystems and imports the RobotMap, Constants and CanSparkMaxPIDWrapper
+SubsystemBase.
+
+It initializes Launcher1 and Launcher2 and will run velocity PID on it.
 
 ------------------------------------------------------------------------------*/
 
 package frc.robot.subsystems;
 
-import frc.robot.utils.RobotMap;
+import frc.robot.utils.RobotMap; 
 import frc.robot.utils.Constants;
 import frc.robot.utils.CANSparkMaxPIDWrapper;
 
@@ -16,13 +22,10 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class LauncherSubsystem extends SubsystemBase {
   
-  
   CANSparkMaxPIDWrapper Launcher1;
   CANSparkMaxPIDWrapper Launcher2;
 
   public LauncherSubsystem() {
-
-
 
   }
 
