29dabf09adcdecc5746423110da8952c6c2527b5
ricozhuthegreat <ricozhuthegreat@hotmail.com>
2020-01-14 19:11:11 -0500

    Updated LimelightSubsystem.java
     - Moved initialization code to constructor
     - Updated years in documentation

--- InfiniteRecharge2020/src/main/java/frc/robot/subsystems/LimelightSubsystem.java
+++ InfiniteRecharge2020/src/main/java/frc/robot/subsystems/LimelightSubsystem.java
@@ -1,6 +1,6 @@
 /*----------------------------------------------------------------------------
 
-Author: Rico Zhu, Emily DiLauro and Nolan Smyth              Date: January 14th, 2019
+Author: Rico Zhu, Emily DiLauro and Nolan Smyth              Date: January 14th, 2020
 
 LimelightSubsystem Class
 
@@ -15,14 +15,22 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class LimelightSubsystem extends SubsystemBase {
 
-  // Network table for the Limelight camera (contains list of NetworkTableEntries, which are bits of a continuous data stream)
-  NetworkTable Table = NetworkTableInstance.getDefault().getTable("limelight");
-
-  NetworkTableEntry tx = Table.getEntry("tx");
-  NetworkTableEntry ty = Table.getEntry("ty");
-  NetworkTableEntry ta = Table.getEntry("ta");
+  // NetworkTable for the Limelight camera (contains list of NetworkTableEntries, which are bits of a continuous data stream)
+  NetworkTable Table;
+  // NetworkTable data stream (aka an entry) for horizontal angle displacement, vertical angle displacement, and target area
+  NetworkTableEntry tx;
+  NetworkTableEntry ty;
+  NetworkTableEntry ta;
 
   public LimelightSubsystem() {
+
+    // Initialize the NetworkTable for the limelight
+    Table = NetworkTableInstance.getDefault().getTable("limelight");
+
+    // Read the data stream for horizontal angle displacement, vertical angle displacement, and target area
+    tx = Table.getEntry("tx");
+    ty = Table.getEntry("ty");
+    ta = Table.getEntry("ta");
 
   }
 
