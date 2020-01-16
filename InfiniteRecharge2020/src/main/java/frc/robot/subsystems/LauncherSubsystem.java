aae0fa911bef502c836f5a54b4453d4322ba3dfa
ricozhuthegreat <ricozhuthegreat@hotmail.com>
2020-01-16 13:07:43 -0500

    Updated LauncherSubsystem.java
     - Updated motor controller devices in the LauncherSubsystem.java class to be TalonFX (for Falcon500s) instead of Rev NEOs

--- InfiniteRecharge2020/src/main/java/frc/robot/subsystems/LauncherSubsystem.java
+++ InfiniteRecharge2020/src/main/java/frc/robot/subsystems/LauncherSubsystem.java
@@ -5,7 +5,7 @@ Author: Rico Zhu, Emily DiLauro and Nolan Smyth              Date: January 13th,
 LauncherSubsystem Class
 
 This is a launcher subsytem called the LancherSubsystem.
-It packages the frc.robot.subsystems and imports the RobotMap, Constants and CanSparkMaxPIDWrapper
+It packages the frc.robot.subsystems and imports the RobotMap, Constants and TalonFX
 SubsystemBase.
 
 It initializes Launcher1 and Launcher2 and will run velocity PID on it.
@@ -16,14 +16,16 @@ package frc.robot.subsystems;
 
 import frc.robot.utils.RobotMap; 
 import frc.robot.utils.Constants;
-import frc.robot.utils.CANSparkMaxPIDWrapper;
+
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.motorcontrol.ControlMode;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class LauncherSubsystem extends SubsystemBase {
-  
-  CANSparkMaxPIDWrapper Launcher1;
-  CANSparkMaxPIDWrapper Launcher2;
+
+  TalonFX Launcher1 = new TalonFX(RobotMap.Launcher1);
+  TalonFX Launcher2 = new TalonFX(RobotMap.Launcher2);
 
   public LauncherSubsystem() {
 
