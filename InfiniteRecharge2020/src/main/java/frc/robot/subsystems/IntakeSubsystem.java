9f5bf36fa246d52ae79f1322c1f3ec2171cb25a7
ricozhuthegreat <ricozhuthegreat@hotmail.com>
2020-01-27 14:04:47 -0500

    Created IntakeSubsystem.java
     - Created a new Subsystem called IntakeSubsystem that both extends the SubsystemBase Abstract class as well as the Subsystem Interface

---
+++ InfiniteRecharge2020/src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -0,0 +1,23 @@
+/*----------------------------------------------------------------------------
+
+Author: Rico Zhu                Date: January 27th, 2020
+
+IntakeSubsystem Class
+
+This subsystem will function for an intake powered by a NEO 550 Brushless Motor.
+
+------------------------------------------------------------------------------*/
+
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class IntakeSubsystem extends SubsystemBase implements Subsystem {
+
+  public IntakeSubsystem() {
+
+  }
+
+}
